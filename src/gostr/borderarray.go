2e4c5de88420c1300e0a06cfdc4aca7ed9f53379
Thomas Mailund <mailund@birc.au.dk>
2021-05-28 08:45:21 +0200

    Computing border arrays

---
+++ src/gostr/borderarray.go
@@ -0,0 +1,31 @@
+package gostr
+
+func Borderarray(x string) []int {
+	ba := make([]int, len(x))
+	for i := 1; i < len(x); i++ {
+		b := ba[i - 1]
+		for ; b > 0 && x[b] != x[i]; b = ba[b-1] {}
+		if x[b] == x[i] {
+			ba[i] = b + 1
+		} else {
+			ba[i] = 0
+		}
+	}
+	return ba
+}
+
+func StrictBorderarray(x string) []int {
+	ba := Borderarray(x)
+	strict := make([]int, len(x))
+	for i := 0; i < len(x); i++ {
+		// I'm handling the last index inside the loop
+		// so I don't have to deal with empty strings
+		// outside of the loop.
+		if i == len(x)-1 || x[ba[i]] != x[i + 1] {
+			strict[i] = ba[i]
+		} else {
+			strict[i] = strict[ba[i]]
+		}
+	}
+	return strict
+}
---
+++ src/gostr/borderarray_test.go
@@ -0,0 +1,46 @@
+package gostr
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBorderarray(t *testing.T) {
+	tests := []struct {
+		name string
+		x    string
+		want []int
+	}{
+		{"(empty string)", "", []int{}},
+		{"a", "a", []int{0}},
+		{"aaa", "aaa", []int{0, 1, 2}},
+		{"aaaba", "aaaba", []int{0, 1, 2, 0, 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Borderarray(tt.x); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Borderarray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStrictBorderarray(t *testing.T) {
+	tests := []struct {
+		name string
+		x string
+		want []int
+	}{
+		{"(empty string)", "", []int{}},
+		{"a", "a", []int{0}},
+		{"aaa", "aaa", []int{0, 0, 2}},
+		{"aaaba", "aaaba", []int{0, 0, 2, 0, 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := StrictBorderarray(tt.x); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("StrictBorderarray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
