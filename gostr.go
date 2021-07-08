3b9cdded14450573bea9679e13a5961bc8d82295
Thomas Mailund <mailund@birc.au.dk>
2021-07-08 08:33:18 +0200

    fuck, forgot to add new file

---
+++ gostr.go
@@ -0,0 +1,12 @@
+package gostr
+
+// ReverseString returns a new string that is the reverse
+// of the input string.
+func ReverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
+}
